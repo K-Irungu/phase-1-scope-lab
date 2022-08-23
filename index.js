0a07992a2611802327b2ad9f7a4a5a0f1cf32f0c
Kevin Irungu <kevin.irungu@student.moringaschool.com>
2022-08-23 11:10:13 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob";
+var bestCustomer;
+
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "someInitialValue";
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "someNewValue";
+}
+
+
+
+
+
